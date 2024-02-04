7dc1b81d8dac687ad84cd76bcf27b04b7c2c2ba2
Turtletnt5 <mdturtleman@gmail.com>
2024-02-04 13:30:34 -0500

    finished check for block out of bounds for falling block grid

--- Grid/MainGridSystem.gd
+++ Grid/MainGridSystem.gd
@@ -71,6 +71,7 @@ func FBGRead(col: int, row: int):
 #function to rotate falling block grid clockwise
 func FBGRotateRight():
 	#creating and filling store new data location inbetween transition
+	@warning_ignore("unassigned_variable")
 	var temp : Array[String]
 	temp.resize(FBGColSize*FBGRowSize)
 	temp.fill("empty")
@@ -80,7 +81,36 @@ func FBGRotateRight():
 		for c in range(FBGColSize):
 			temp[(FBGColSize * (FBGRowSize - c - 1) ) + r] = FBGRead(c, r)
 	
+	#setting the grid to the new locations
 	FallingBlockGrid = temp
+	
+	#check for blocks out of bounds and move the FBGrid to solve
+	checkFBGridOutOfBounds()
+
+#cheacks to see if any FBGrid blocks are out of bounds. if there are it moves the FBGrid to solve the issue
+func checkFBGridOutOfBounds():
+	
+	#checks if any colums are out of bound on left side
+	if( FBGReferenceLocation.x < 0):
+		#finds how many cols are out of bounds and cheacks to see if any blocks are out side
+		var colOutOfBounds = FBGReferenceLocation.x * -1
+		for c in range(colOutOfBounds):
+			for r in range(FBGRowSize):
+				#if a block is found to be outside move the referance point over to garenty its not
+				if(FBGRead(c,r) != "empty"):
+					FBGReferenceLocation.x += colOutOfBounds
+	
+	#checks if any coms are out of bounds on the right side
+	if(FBGReferenceLocation.x + FBGColSize - 1 >= MGColSize):
+		#finds how many cols are out of bounds and cheacks to see if any blocks are out side
+		var colOutOfBounds = FBGReferenceLocation.x + FBGColSize - MGColSize + 1
+		for c in range(FBGColSize -1, colOutOfBounds -1, -1):
+			for r in range(FBGRowSize):
+				#if a block is found to be outside move the referance point over to garenty its not
+				if(FBGRead(c,r) != "empty"):
+					FBGReferenceLocation.x -= colOutOfBounds
+		
+		
 
 
 
