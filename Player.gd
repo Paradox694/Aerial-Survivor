d0b36930aee6fbce082e03d7d6781801bc1a8473
Zanutel22 <zanutel22@gmail.com>
2024-02-05 15:59:47 -0500

    Added additional comments to the movement code

--- Player.gd
+++ Player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
+#constants for movement 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400
 
+# Get default gravity from project settings
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
@@ -17,8 +19,11 @@ func _physics_process(delta):
 	#Handle input for left and right movement 
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
+		# Move right if direction is positive, move left if negative
 		velocity.x = direction * SPEED
 	else:
+		 # Slow down if no input for smoother movement
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	  # Move the character based on the calculated velocity
 	move_and_slide()
