5823ea5ee18eaafb030d97ea7dc42e146d642380
Ty Brontmeyer <tbrontmeyer@gmail.com>
2024-03-01 16:51:26 -0500

    7 blocks working (I, L, T, J, O, Z, S)

--- scripts/DrawShape.gd
+++ scripts/DrawShape.gd
@@ -9,47 +9,52 @@ var row: int
 
 var shape1 : Array [int] = [0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
 var shape2 : Array [int] = [0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0]
+var shape3 : Array [int] = [0, 0, 0, 0, 3, 3, 3, 0, 0, 3, 0, 0, 0, 0, 0, 0]
+var shape4 : Array [int] = [0, 0, 0, 0, 0, 4, 4, 4, 0, 4, 0, 0, 0, 0, 0, 0]
+var shape5 : Array [int] = [0, 0, 0, 0, 0, 5, 5, 0, 0, 5, 5, 0, 0, 0, 0, 0]
+var shape6 : Array [int] = [0, 0, 0, 0, 0, 6, 6, 0, 6, 6, 0, 0, 0, 0, 0, 0]
+var shape7 : Array [int] = [0, 0, 0, 0, 0, 7, 7, 0, 0, 0, 7, 7, 0, 0, 0, 0]
 
 func _draw():
 	
 	match shapeToDraw:
-		1: # "Line" shape
+		1: # I-shape
 			draw_texture(textureArray[0], Vector2 (40, 112))
 			draw_texture(textureArray[0], Vector2 (72, 112))
 			draw_texture(textureArray[0], Vector2 (104, 112))
 			draw_texture(textureArray[0], Vector2 (136, 112))
 		
-		2: # Right "L" shape
+		2: # L-shape
 			draw_texture(textureArray[1], Vector2 (48, 112))
 			draw_texture(textureArray[1], Vector2 (80, 112))
 			draw_texture(textureArray[1], Vector2 (112, 112))
 			draw_texture(textureArray[1], Vector2 (112, 80))
 		
-		3: # "T" shape
+		3: # T-shape
 			draw_texture(textureArray[2], Vector2 (80, 80))
 			draw_texture(textureArray[2], Vector2 (80, 112))
 			draw_texture(textureArray[2], Vector2 (112, 112))
 			draw_texture(textureArray[2], Vector2 (48, 112))
 			
-		4: # Left "L" shape
+		4: # J-shape
 			draw_texture(textureArray[3], Vector2 (48, 80))
 			draw_texture(textureArray[3], Vector2 (80, 112))
 			draw_texture(textureArray[3], Vector2 (112, 112))
 			draw_texture(textureArray[3], Vector2 (48, 112))
 			
-		5: # Box shape
+		5: # O-shape
 			draw_texture(textureArray[4], Vector2 (64, 80))
 			draw_texture(textureArray[4], Vector2 (96, 80))
 			draw_texture(textureArray[4], Vector2 (96, 112))
 			draw_texture(textureArray[4], Vector2 (64, 112))
 			
-		6: # Left "S" shape
+		6: # Z-shape
 			draw_texture(textureArray[5], Vector2 (48, 80))
 			draw_texture(textureArray[5], Vector2 (80, 80))
 			draw_texture(textureArray[5], Vector2 (80, 112))
 			draw_texture(textureArray[5], Vector2 (112, 112))
 			
-		7: # Right "S" Shape
+		7: # S-Shape
 			draw_texture(textureArray[6], Vector2 (48, 112))
 			draw_texture(textureArray[6], Vector2 (80, 80))
 			draw_texture(textureArray[6], Vector2 (80, 112))
@@ -58,11 +63,26 @@ func _draw():
 			
 func NextShape():
 	match shapeToDraw:
-		1: 
+		1: # I-shape
 			changeFBGArray.emit(shape1)
 			shapeToDraw = 2
-		2:
+		2: # L-shape
 			changeFBGArray.emit(shape2)
+			shapeToDraw = 3
+		3: # T-shape
+			changeFBGArray.emit(shape3)
+			shapeToDraw = 4
+		4: # J-shape
+			changeFBGArray.emit(shape4)
+			shapeToDraw = 5
+		5: # O-shape
+			changeFBGArray.emit(shape5)
+			shapeToDraw = 6
+		6: # Z-shape
+			changeFBGArray.emit(shape6)
+			shapeToDraw = 7
+		7: # S-shape
+			changeFBGArray.emit(shape7)
 			shapeToDraw = 1
 	queue_redraw()
 
