f84867ab21d17a91a9fa3db948e91aeb4eb02d9f
Zanutel22 <zanutel22@gmail.com>
2024-03-03 20:06:20 -0500

    Added a vloume slider to the MainMenu options menu to control the volume of the master volume Added the sprites to the pause menu

--- PauseMenu/PauseMenu.gd
+++ PauseMenu/PauseMenu.gd
@@ -3,8 +3,7 @@ extends Control
 @onready var main = $"../"
 
 func _on_resume_pressed():
-		main.pauseMenu()
-
+	main.pauseMenu()
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://PauseMenu/OptionsPauseMenu.tscn")
@@ -12,6 +11,5 @@ func _on_options_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
-
 
 
