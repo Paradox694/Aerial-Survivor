489e90a2158766c4ecb6b9ed6cd4c8b550b93850
Paradox694 <nathan.irvine2003@gmail.com>
2024-03-04 23:32:56 -0500

    Attempt to fill in multiple rows

--- Water/Water.gd
+++ Water/Water.gd
@@ -6,23 +6,32 @@ signal fillRow(fillType, row)
 
 @onready var polygon_2d = $Polygon2D
 
+@onready var timer = $Timer
+
+var fillType = 1
+	
+var row = 0
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(polygon_2d.position.y)
+	#print(polygon_2d.position.y)
+	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(polygon_2d.position.y < -540):
+	# original way of moving the water
+	#if(polygon_2d.position.y < -540):
+		#emit_signal("stop")
+	
+	if(timer.time_left == 1.0):
+		row += 1
+	
+	if(row == 16):
 		emit_signal("stop")
 
 func _on_water_timer_rise():
 	# original way of moving the water
 	#polygon_2d.position.y = polygon_2d.position.y - 0.5
 	
-	var fillType = 1
-	
-	var row = 0
-	
 	fillRow.emit(fillType, row)
-	
 	print("The water is rising")
