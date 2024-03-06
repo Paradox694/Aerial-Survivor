9fef9c5de5e03c77351c608637abcecf79de784d
Paradox694 <nathan.irvine2003@gmail.com>
2024-03-06 11:26:09 -0500

    The end to the branch

--- Water/Water.gd
+++ Water/Water.gd
@@ -6,7 +6,7 @@ signal fillRow(fillType, row)
 
 @onready var polygon_2d = $Polygon2D
 
-@onready var timer = $Timer
+@export var maxWaterHight : int = 19
 
 var fillType = 1
 	
@@ -14,24 +14,32 @@ var row = 0
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
 	#print(polygon_2d.position.y)
-	timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
 	# original way of moving the water
 	#if(polygon_2d.position.y < -540):
 		#emit_signal("stop")
 	
-	if(timer.time_left == 1.0):
-		row += 1
+	#if(timer.time_left == 1.0):
+		#row += 1
 	
-	if(row == 16):
-		emit_signal("stop")
+	#if(row == maxWaterHight - 1):
+		#emit_signal("stop")
 
-func _on_water_timer_rise():
+func WaterRise(Levels : int):
 	# original way of moving the water
 	#polygon_2d.position.y = polygon_2d.position.y - 0.5
 	
-	fillRow.emit(fillType, row)
-	print("The water is rising")
+	for r in range(Levels):
+		row += 1
+		
+		if(row >=maxWaterHight):
+			stop.emit()
+			break
+		
+		fillRow.emit(fillType, row)
+		print("The water is rising")
