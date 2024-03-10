18aa33679cfb1e0118952247b1bd84ca1117301b
Paradox694 <nathan.irvine2003@gmail.com>
2024-03-09 21:59:07 -0500

    Added collision box movement

---
+++ Water Damage/Water_Damage.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+signal stop
+
+@onready var collision_shape_2d = $CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(collision_shape_2d.position.y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(collision_shape_2d.position.y < -400):
+		print(collision_shape_2d.position.y)
+		emit_signal("stop")
+
+func _on_water_timer_rise():
+	print(collision_shape_2d.position.y)
+	collision_shape_2d.position.y = collision_shape_2d.position.y - 0.5
